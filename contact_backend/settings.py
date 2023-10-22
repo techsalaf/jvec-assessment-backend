0c9a53ca825b7df5bf094b7196c35110a44d8efa
techsalaf <amudarash102@gmail.comM>
2023-10-22 10:23:36 +0100

    Added a Simple JWT Authentication to settings

--- contact_backend/settings.py
+++ contact_backend/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contacts',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
