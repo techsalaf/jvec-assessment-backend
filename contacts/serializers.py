0e757bb954f0562fb532fb26841bc4b7b361bcb0
techsalaf <amudarash102@gmail.comM>
2023-10-22 10:35:23 +0100

    Added validation logic to the serializers.py file.

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -7,3 +7,9 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = '__all__'
+
+    def validate_phone_number(self, value):
+        # Add custom validation for phone number
+        if not value.startswith('+'):
+            raise serializers.ValidationError("Phone number must start with '+'")
+        return value
