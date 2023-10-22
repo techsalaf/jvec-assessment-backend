03cf0769b4748f65f3b11d81fc08859b0a541e3b
techsalaf <amudarash102@gmail.comM>
2023-10-22 10:37:21 +0100

    Use the djoser library to handle authentication endpoints:

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,9 +4,13 @@
 # contacts/urls.py
 
 from django.urls import path
+from djoser import views as djoser_views
+from rest_framework_simplejwt import views as jwt_views
 from .views import ContactListCreateView, ContactRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('contacts/', ContactListCreateView.as_view(), name='contact-list-create'),
     path('contacts/<int:pk>/', ContactRetrieveUpdateDestroyView.as_view(), name='contact-retrieve-update-destroy'),
+    path('auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
